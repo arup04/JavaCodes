4e2eb5a26bca98e91b6e2185a2854a120fc48d4b
Arup Das <156603544+arup04@users.noreply.github.com>
2024-11-23 00:35:20 +0530

    Add files via upload

---
+++ Rect_area.java
@@ -0,0 +1,22 @@
+package com.gsv.project;
+class Square
+{
+    int Side;
+    public int area()
+    {
+        return Side*Side;
+    }
+    public int perimeter()
+    {
+        return 4*Side;
+    }
+}
+public static void main(String[] args)
+{
+    public class Rect_area {
+    Square sq = new Square();
+    sq.Side = 4;
+    System.out.println(Sq.area());
+    System.out.println(sq.perimeter());
+}
+}
